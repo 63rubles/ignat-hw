af924f0c6ff9a445b384b9690bc4b39a44b2e718
sk3llytoon <hatsz141415@gmail.com>
2022-07-12 16:18:04 +0300

    Ignat HW3 - done

--- src/p2-homeworks/h3/Greeting.tsx
+++ src/p2-homeworks/h3/Greeting.tsx
@@ -1,12 +1,13 @@
-import React from 'react'
+import React, {ChangeEvent} from 'react'
 import s from './Greeting.module.css'
+import {UserType} from "./HW3";
 
 type GreetingPropsType = {
-    name: any // need to fix any
-    setNameCallback: any // need to fix any
-    addUser: any // need to fix any
-    error: any // need to fix any
-    totalUsers: any // need to fix any
+    name: string // need to fix any
+    setNameCallback: (e: ChangeEvent<HTMLInputElement>)=>void // need to fix any
+    addUser: ()=>void // need to fix any
+    error: string | null // need to fix any
+    totalUsers: number // need to fix any
 }
 
 // презентационная компонента (для верстальщика)
@@ -17,10 +18,18 @@ const Greeting: React.FC<GreetingPropsType> = (
 
     return (
         <div>
-            <input value={name} onChange={setNameCallback} className={inputClass}/>
-            <span>{error}</span>
+            <input value={name}
+                   onChange={setNameCallback}
+                   className={error ? inputClass : ''}/>
+            {/*{error && <div className={s.errorMessage}>{error}</div>}*/}
+            {/*{error && <span className={s.errorMessage}>{error}</span>}*/}
+            {/*<span>{error}</span>*/}
             <button onClick={addUser}>add</button>
             <span>{totalUsers}</span>
+            <div>
+                {error && <span className={s.errorMessage}>{error}</span>}
+            </div>
+
         </div>
     )
 }
